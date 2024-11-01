40ebb5955c6447e38bb1b7f3b587ab5c8c33ca79
Bangaru Suresh <sureshgpu@gmail.com>
2024-11-01 19:17:44 +0530

    clear explain line by line

--- threads_processes/creating_processes.py
+++ threads_processes/creating_processes.py
@@ -4,15 +4,16 @@ from multiprocessing import Process
 
 
 def do_work():
-    print("Starting work")
+    print("Start Work")
     i = 0
     for _ in range(20000000):
-        i += 1
-    print("Finished work")
+        i+=1
+
+    print("Finsihed Work")
 
 
-if __name__ == '__main__':
-    multiprocessing.set_start_method('spawn')
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
     for _ in range(5):
         p = Process(target=do_work, args=())
         p.start()
--- threads_processes/creating_threads.py
+++ threads_processes/creating_threads.py
@@ -1,14 +1,69 @@
-import time
-from threading import Thread
+# here we are creating our first Thread.
+# before going to thred using time.sleep()
 
+import time
 
 def do_work():
-    print("Starting work")
-    time.sleep(1)
-    print("Finished work")
+    print("Start Work")
+    time.sleep(1) # this means wait for 1 secounds.
+    print("Finshed Work")
+
+for _ in range(5):
+    do_work()
+
+# we are getting like this output.
+# here code excuted line by line.
+# here process complete line by line.
+'''
+Start Work
+Finshed Work
+Start Work
+Finshed Work
+Start Work
+Finshed Work
+Start Work
+Finshed Work
+Start Work
+Finshed Work
+
+'''
+
+
+######################################################
+print("\n\n")
+print("With using Thread Concept")
+# With using Thread Concept.
+
+from threading import Thread
+
+def do_work():
+
+    print("Start Work")
+    time.sleep(1) # this means wait for 1 secounds.
+    i=0
+    for _ in range(20000000):
+        i+=1
+    print("Finshed Work")
 
 
 for _ in range(5):
     t = Thread(target=do_work, args=())
     t.start()
+
+# we are getting like this output with using Thread.
+# here complete 1 line and 1 process and all iterations.
+
+'''
+Start Work
+Start Work
+Start Work
+Start Work
+Start Work
+Finshed Work
+Finshed Work
+Finshed Work
+Finshed Work
+Finshed Work
+
+'''
 
